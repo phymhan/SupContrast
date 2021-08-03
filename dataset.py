f438c2885c6c82ef2d31245a1aedf314adc40f99
Seungwook Han <sh3264@coumbia.edu>
2021-08-03 19:01:22 -0400

    Fixed stacking

--- dataset.py
+++ dataset.py
@@ -41,7 +41,7 @@ class ClassDataset(Dataset):
         # Sample images from respective classes
         for i in range(len(parts)):
             idxs = np.random.choice(self.label_to_idxs[int(labels[i])], parts[i])
-            x.append([torch.stack(self.data[idx], dim=0) for idx in idxs])
+            x.append([torch.stack(self.data[idx][0], dim=0) for idx in idxs])
         
         return torch.cat(x, dim=0)
 
