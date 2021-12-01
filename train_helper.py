08cf1004a2f172e5b9b95af7a10470cc3d26027c
Seungwook Han <sh3264@coumbia.edu>
2021-12-01 11:36:34 -0500

    Removing color jitter & grayscale

--- train_helper.py
+++ train_helper.py
@@ -491,10 +491,6 @@ class Transform:
         self.transform_cmnist = transforms.Compose([
             transforms.RandomResizedCrop(size=28, scale=(0.2, 1.)),
             transforms.RandomHorizontalFlip(),
-            transforms.RandomApply([
-                transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
-            ], p=0.8),
-            transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             # transforms.Normalize(mean=[0.485, 0.456, 0.406],
                         # std=[0.229, 0.224, 0.225])
