0da30bc39ca98bc461a2c4ff2b8c2924d263f315
Seungwook Han <sh3264@coumbia.edu>
2022-01-31 17:28:49 -0500

    Autoaugment script

---
+++ scripts/run_supcon_aimos_multi_autoaugment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## User python environment
+HOME2=/gpfs/u/home/BNSS/BNSSsgwh
+PYTHON_VIRTUAL_ENVIRONMENT=torch1.7
+CONDA_ROOT=$HOME2/barn/miniconda3
+
+## Activate WMLCE virtual environment 
+source ${CONDA_ROOT}/etc/profile.d/conda.sh
+conda activate $PYTHON_VIRTUAL_ENVIRONMENT
+ulimit -s unlimited
+
+python \
+      $HOME2/scratch/baseline/SupContrast/main_dist_multi.py \
+      --data /gpfs/u/locker/200/CADS/datasets/ImageNet/train/ \
+      --workers 24 \
+      --nodes 4 \
+      --ngpus 6 \
+      --epochs 800 \
+      --batch-size 1536 \
+      --learning-rate 1.2 \
+      --temp 0.1 \
+      --print-freq 50 \
+      --autoaugment \
+      --checkpoint-dir $HOME2/scratch/baseline/SupContrast/results/ \
+      --log-dir $HOME2/scratch/baseline/SupContrast/logs/ \
+      --name supcon-baseline-imagenet-multi-autoaugment
+
+echo "Run completed at:- "
+date
+
