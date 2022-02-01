24a15a925e1f37aa8f572ca3df6cdd054bdf268d
Han <BNSSsgwh@dcsfen01.ccni.rpi.edu>
2022-02-01 15:20:15 -0500

    Fixed conda path

--- scripts/run_supcon_aimos_multi_autoaugment.sh
+++ scripts/run_supcon_aimos_multi_autoaugment.sh
@@ -3,7 +3,7 @@
 ## User python environment
 HOME2=/gpfs/u/home/BNSS/BNSSsgwh
 PYTHON_VIRTUAL_ENVIRONMENT=pytorch1.7
-CONDA_ROOT=$HOME2/barn/miniconda3
+CONDA_ROOT=$HOME/scratch/miniconda3
 
 ## Activate WMLCE virtual environment 
 source ${CONDA_ROOT}/etc/profile.d/conda.sh
