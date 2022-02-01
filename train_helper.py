d57370632296523e2420c2ae85f03b5a2f0d75d3
Seungwook Han <sh3264@coumbia.edu>
2022-02-01 15:32:18 -0500

    Fixed autoaugment for OFF mode

--- train_helper.py
+++ train_helper.py
@@ -426,7 +426,6 @@ class Transform:
                 transforms.RandomResizedCrop(size=224, scale=(0.08, 1.)),
                 transforms.RandomHorizontalFlip(),
                 ImageNetPolicy(), 
-                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET, interpolation=transforms.INTERPOLATIONMODE.BILINEAR),
                 #transforms.RandomApply([transforms.GaussianBlur(kernel_size=224//20*2+1, sigma=(0.1, 2.0))], p=0.5),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -437,8 +436,6 @@ class Transform:
             self.transform_supcon = transforms.Compose([
                 transforms.RandomResizedCrop(size=224, scale=(0.08, 1.)),
                 transforms.RandomHorizontalFlip(),
-                ImageNetPolicy(), 
-                # transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET, interpolation=transforms.INTERPOLATIONMODE.BILINEAR),
                 #transforms.RandomApply([transforms.GaussianBlur(kernel_size=224//20*2+1, sigma=(0.1, 2.0))], p=0.5),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
